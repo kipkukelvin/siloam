a6132041b83ac5b74c8dc8e52b27d4c16e407929
Kelvin <kipkukelvin1@gmail.com>
2026-03-17 12:56:36 +0300

    Gallery Update

--- resources/views/school/gallery.blade.php
+++ resources/views/school/gallery.blade.php
@@ -5,7 +5,7 @@
 
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 
-    <section class="py-5 bg-light">
+    <section class="py-5 mt-3 bg-light">
         <div class="container">
 
             <h1 class="text-center hf fw-bold mb-4">Media Library</h1>
@@ -16,47 +16,79 @@
             <div class="row g-4">
 
                 <!-- Image -->
-                <div class="col-md-4 col-sm-6">
-                    <div class="gallery-item">
+                <div class="col-md-3 col-sm-6 mb-2">
+                    <div class="gallery-item position-relative overflow-hidden shadow-sm">
                         <a href="{{ asset('assets/Gallery/Siloam.jpeg') }}" data-lightbox="gallery">
-                            <img src="{{ asset('assets/Gallery/Siloam.jpeg') }}" class="img-fluid rounded shadow-sm">
+                            <img src="{{ asset('assets/Gallery/Siloam.jpeg') }}" class="img-fluid w-100" loading="lazy">
+                            <div class="gallery-caption">
+                                <span class="fw-bold">Siloam College</span>
+                            </div>
                         </a>
                     </div>
                 </div>
 
-                <div class="col-md-4 col-sm-6">
-                    <div class="gallery-item">
+                <div class="col-md-3 col-sm-6 mb-2">
+                    <div class="gallery-item position-relative overflow-hidden shadow-sm">
                         <a href="{{ asset('assets/Gallery/Library.jpg') }}" data-lightbox="gallery">
-                            <img src="{{ asset('assets/Gallery/Library.jpg') }}" class="img-fluid rounded shadow-sm">
+                            <img src="{{ asset('assets/Gallery/Library.jpg') }}" class="img-fluid w-100" loading="lazy">
+                            <div class="gallery-caption">
+                                <span class="fw-bold">Library</span>
+                            </div>
                         </a>
                     </div>
                 </div>
 
-                <div class="col-md-4 col-sm-6">
-                    <div class="gallery-item">
+                <div class="col-md-3 col-sm-6 mb-2">
+                    <div class="gallery-item position-relative overflow-hidden shadow-sm">
                         <a href="{{ asset('assets/Gallery/Skillslab.jpg') }}" data-lightbox="gallery">
-                            <img src="{{ asset('assets/Gallery/Skillslab.jpg') }}" class="img-fluid rounded shadow-sm">
+                            <img src="{{ asset('assets/Gallery/Skillslab.jpg') }}" class="img-fluid w-100" loading="lazy">
+                            <div class="gallery-caption">
+                                <span class="fw-bold">Skills Lab</span>
+                            </div>
                         </a>
                     </div>
                 </div>
-                <div class="col-md-4 col-sm-6">
-                    <div class="gallery-item">
+
+                <div class="col-md-3 col-sm-6 mb-2">
+                    <div class="gallery-item position-relative overflow-hidden shadow-sm">
                         <a href="{{ asset('assets/Gallery/Students.jpg') }}" data-lightbox="gallery">
-                            <img src="{{ asset('assets/Gallery/Students.jpg') }}" class="img-fluid rounded shadow-sm">
+                            <img src="{{ asset('assets/Gallery/Students.jpg') }}" class="img-fluid w-100" loading="lazy">
+                            <div class="gallery-caption">
+                                <span class="fw-bold">Students</span>
+                            </div>
                         </a>
                     </div>
                 </div>
-                <div class="col-md-4 col-sm-6">
-                    <div class="gallery-item">
+
+                <div class="col-md-3 col-sm-6 mb-2">
+                    <div class="gallery-item position-relative overflow-hidden shadow-sm">
                         <a href="{{ asset('assets/Gallery/Welcome.jpg') }}" data-lightbox="gallery">
-                            <img src="{{ asset('assets/Gallery/Welcome.jpg') }}" class="img-fluid rounded shadow-sm">
+                            <img src="{{ asset('assets/Gallery/Welcome.jpg') }}" class="img-fluid w-100" loading="lazy">
+                            <div class="gallery-caption">
+                                <span class="fw-bold">Welcome</span>
+                            </div>
                         </a>
                     </div>
                 </div>
-                <div class="col-md-4 col-sm-6">
-                    <div class="gallery-item">
+                
+                <div class="col-md-3 col-sm-6 mb-2">
+                    <div class="gallery-item position-relative overflow-hidden shadow-sm">
                         <a href="{{ asset('assets/Gallery/Classroom.jpg') }}" data-lightbox="gallery">
-                            <img src="{{ asset('assets/Gallery/Classroom.jpg') }}" class="img-fluid rounded shadow-sm">
+                            <img src="{{ asset('assets/Gallery/Classroom.jpg') }}" class="img-fluid w-100" loading="lazy">
+                            <div class="gallery-caption">
+                                <span class="fw-bold">Classroom</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 mb-2">
+                    <div class="gallery-item position-relative overflow-hidden shadow-sm">
+                        <a href="{{ asset('assets/images/soccer.jpg') }}" data-lightbox="gallery">
+                            <img src="{{ asset('assets/images/soccer.jpg') }}" class="img-fluid w-100" loading="lazy">
+                            <div class="gallery-caption">
+                                <span class="fw-bold">Sports</span>
+                            </div>
                         </a>
                     </div>
                 </div>
